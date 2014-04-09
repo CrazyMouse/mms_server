60b239840d70b1928a1de9446aa9ab256f2dece2
tanyangc <tan.yang86@gmail.com>
2014-04-09 15:40:27 +0800

    日志调整

--- src/main/java/com/crazymouse/mmsserver/MmsMain.java
+++ src/main/java/com/crazymouse/mmsserver/MmsMain.java
@@ -1,5 +1,8 @@
 package com.crazymouse.mmsserver;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,6 +12,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Create Time: 14-4-4 下午2:25
  */
 public class MmsMain {
+    static {
+        String logbackCfg = "./conf/logback.xml";
+        try {
+            // 初始化log配置
+            LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+            JoranConfigurator configurator = new JoranConfigurator();
+            configurator.setContext(lc);
+            lc.reset();
+            configurator.doConfigure(logbackCfg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:/conf/applicationContext.xml");
     }
